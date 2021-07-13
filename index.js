93dbb62b0943575df36634c52bb342ee92fa7780
OahidZihad <oahidzihad1@gmail.com>
2021-07-13 19:06:34 +0600

    add all products from fakeData to mongoDB database

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+const { MongoClient } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9wgmh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 const port = 4000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const productsCollection = client.db("emaJohnStore").collection("products");
+
+  app.post("/addProduct", (req, res) => {
+    const products = req.body;
+    productsCollection.insertMany(products).then((result) => {
+      result.send(result.insertedCount);
+      console.log(result.insertedCount);
+    });
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+app.listen(port);
